352d589ee0564c99895344b28241e671ab85b72e
João Alli <jpsousa.alli@gmail.com>
2025-03-25 16:31:00 -0300

    aula sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+     ECHO "Hello Word!<br>";
+     echo "Hello Word!<br>";
+     EcHo "Hello Word!<br>";
+
+     $nome = "João";
+     $sobrenome = "Alli";
+     $numero = 4;
+     
+    echo $nome . "lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p>" . $nome . 
+    " " . $sobrenome  . "</p>";
+?>
